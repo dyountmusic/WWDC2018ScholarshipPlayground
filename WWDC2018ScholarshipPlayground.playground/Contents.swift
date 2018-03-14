8916acad0c4c63a0820539d00920bba62334dd04
Daniel Yount <yountdaniel@gmail.com>
2018-03-14 02:27:26 -0400

    Playing around a bit.

--- WWDC2018ScholarshipPlayground.playground/Contents.swift
+++ WWDC2018ScholarshipPlayground.playground/Contents.swift
@@ -1,21 +1,40 @@
-//: A UIKit based Playground for presenting user interface
+//: WWDC 2018 Scholarship Playground
+//: Author: Daniel Yount (yountdaniel@gmail.com)
+//: GitHub: dyountmusic
   
 import UIKit
 
 import PlaygroundSupport
 
-class MyViewController : UIViewController {
+class MainViewController : UIViewController {
+    
+    var name: String {
+        get { return UserDefaults.standard.string(forKey: "name") ?? "" }
+        set { UserDefaults.standard.set(newValue, forKey: "name") }
+    }
     
     override func viewDidLoad() {
         // Do Cool Stuff Here!
+        
     }
     
     override func loadView() {
         let view = UIView()
+        view.backgroundColor = .white
+
+        // Setting
+        name = "Daniel"
+        
+        // Printing
+        print(name)
+        
+        // Checking Disk Value
+        UserDefaults.standard.string(forKey: "name")!
         
         self.view = view
     }
 }
-// Present the view controller in the Live View window
-PlaygroundPage.current.liveView = MyViewController()
+
+
+PlaygroundPage.current.liveView = MainViewController()
 
